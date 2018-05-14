9ad74cb802ad3da0371df3a560b93c669eb64e93
wyang36 <wenqin.z.yang@gmail.com>
2018-05-14 18:37:14 -0400

    course update
    adding google oauth

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config
+require('./config/passport')(passport);
+
+//Load routes
+const auth = require('./routes/auth');
+
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('It works');
-})
+});
+
+//Use routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://wyang36:Unendlichkeit828@ds027749.mlab.com:27749/storybook-dev',
+    googleClientID: '533543234388-uokdnmi4t6p7ve02343u36vgu91l2scc.apps.googleusercontent.com',
+    googleClientSecret: 'E0eE62lCwGEbyocCFq6SfqGL'
+}
